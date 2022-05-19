da6c6a42a9bbada7681a692a717f0481d7fd68f4
David Celis <davidcelis682@gmail.com>
2022-05-18 21:19:50 -0500

    update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname+'/dist/final-angular-guess'));
 app.get('/',function(req,res){
+    console.log()
     res.sendFile(path.join(__dirname+'/dist/final-angular-guess/index.html'));
 });
 
