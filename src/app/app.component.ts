cdc3c282901ae77b456f2e99791c71d3680e9c59
David Celis <davidcelis682@gmail.com>
2022-05-21 16:23:44 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
+import { filter } from 'rxjs/operators';
+
+declare var gtag: (arg0: string, arg1: string, arg2: { page_path: string; }) => void;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'final-angular-guess';
+
+  constructor(
+    private router: Router
+  ) {
+    const navEndEvents$ = this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-8HDXS9V7BR', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  }
 }
